7daec00a398fe9de40d6463823dfd02ab7419447
DomenicoCoppeta <domenicocoppeta@gmail.com>
2023-09-17 20:09:54 +0200

    FINE

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-
-
 function search() {
     const artist = document.getElementById("searchField").value;
-    console.log(artist);
     getMusic(artist)
-}
-
-function displayMusic(albums) {      
-
-    console.log(albums)
-    console.log(albums.data[0].album.title)
-    
-
-   
 }
 
 function getMusic(artist) {
@@ -24,14 +12,34 @@ function getMusic(artist) {
         })
 }
 
+function displayMusic(albums) {    
+    console.log(albums);
+    const albumsRow = document.createElement("div")
+    albumsRow.classList.add("row", "albumsRow");
+    const searchTitle = document.createElement("h2");
+    searchTitle.classList.add("searchTitle");
+    searchTitle.style.color = "white";
+    searchTitle.innerHTML = document.getElementById("searchField").value;
+    const albumResults = document.createElement("div")
+    albumResults.classList.add("col-10", "searchResults")
+    const coversRow = document.createElement("div")
+    coversRow.classList.add("row", "row-cols-1", "row-cols-sm-2", "row-cols-lg-2", "row-cols-xl-4", "imgLinks", "py-3")
+    albumsRow.appendChild(albumResults)
+    document.getElementById("main").appendChild(albumsRow)
+    albumResults.appendChild(searchTitle)
+    albumResults.appendChild(coversRow)
 
-
-
-
-
-
-
-
-
-
-
+    for (let i = 0; i < 4 && i < albums.data.length; i++) {
+        const searchTitle = document.createElement("h5")
+        searchTitle.style.color ="white"
+        searchTitle.classList.add("searchTitle")
+        searchTitle.innerHTML = albums.data[i].album.title;
+        const albumDiv = document.createElement("div")
+        albumDiv.classList.add("col-3")
+        const albumCover = document.createElement("img")
+        albumCover.src = albums.data[i].album.cover
+        coversRow.appendChild(albumDiv) 
+        albumDiv.appendChild(albumCover)
+        albumDiv.appendChild(searchTitle)
+}
+}
